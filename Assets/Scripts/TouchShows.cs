8ebffdb0147dbc13667a1b2fb35b42c3acb4aa66
roman1000 <roman2005.19.07@gmail.com>
2020-11-06 17:47:12 +0200

    first commit

---
+++ Assets/Scripts/TouchShows.cs
@@ -0,0 +1,41 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchShows : MonoBehaviour
+{
+    #region AllParams
+    public Text ShowText;
+    string BoxFallOnPlane = "Box Fall on Plane";
+    string BoxFallOnWall = "Box Fall on Wall";
+    float _timerToNullText = 0;
+    #endregion
+    private void Update()
+    {
+        if (_timerToNullText > 0) _timerToNullText -= 0.04f;
+        else ShowText.text = "";
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Wall>() != null)
+        {
+            _timerToNullText = 0;
+            ShowText.text = BoxFallOnWall;
+            SpawnText();
+        }else if (collision.gameObject.GetComponent<Plane>() != null)
+        {
+            _timerToNullText = 0;
+            ShowText.text = BoxFallOnPlane;
+            SpawnText();
+        }
+    }
+    public void SpawnText()
+    {
+        _timerToNullText = 10;
+    }
+
+}
---
+++ Assets/Scripts/jump.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 400f);
+    }
+}
